84c8be0ec0e64eb4fdaad1b5298899bd55c6b5f0
Lara <lara.faria@tecnico.ulisboa.pt>
2023-12-18 11:04:05 +0000

    scc done

--- proj2.cpp
+++ proj2.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <stack>
 #include <list>
-#define max(a, b) (a > b ? a : b)
 
 using namespace std;
 
@@ -23,13 +22,12 @@ public:
         adjacencyList[u].push_back(v);
     }
 
-
     list<int> DFS(int startVertex) {
         vector<Color> color(vertices + 1, WHITE);
         stack<int> vertexStack;
         list<int> orderedVertices;                           // list to store vertices in descending order of time
 
-        for (int v = 1; v <= vertices; ++v) {
+        for (int v = 1; v <= vertices; v++) {
             if (color[v] == WHITE) {
                 vertexStack.push(v);
 
@@ -56,21 +54,19 @@ public:
         return orderedVertices;
     }
 
-
-    int calculateMaxJump(Graph tpGraph){
+    vector<int> calculate_SCC(list<int> orderedVertices, Graph tpGraph){
         vector<Color> color(vertices + 1, WHITE);
-        vector<int> dp(vertices + 1, 0);                     // stores the max jumps for each subproblem (each vertex)   
         stack<int> vertexStack;
 
-        // do DFS in original graph
-        list<int> orderedVertices = DFS(1);                  // start DFS at vertex 1
+        int scc = 1;
+        int number_vertices_scc = 0;
+        vector<int> vertex_scc (vertices + 1, 0);
 
-        int maxJump = 0;
-        
         // do DFS in transpose graph
         for(const int& v: orderedVertices){
             if (color[v] == WHITE) {
                 vertexStack.push(v);
+
                 while (!vertexStack.empty()) {
                     int currentVertex = vertexStack.top();
                     vertexStack.pop();
@@ -79,24 +75,79 @@ public:
                         color[currentVertex] = GREY;
                         vertexStack.push(currentVertex);
 
+                        vertex_scc[currentVertex] = scc;
+                        number_vertices_scc++;
+
                         for (int adjacentVertex : tpGraph.adjacencyList[currentVertex]) {
                             if (color[adjacentVertex] == WHITE){
                                 vertexStack.push(adjacentVertex);
-                            }   
-                            dp[currentVertex] = max(dp[currentVertex], dp[adjacentVertex] + 1);
+                                this->addConnection(currentVertex, adjacentVertex);
+                            }
+                        }
+                    }
+
+                    else if (color[currentVertex] == GREY) {
+                        color[currentVertex] = BLACK;
+                        number_vertices_scc--;
+                        if(number_vertices_scc == 0){
+                            scc++;
+                        }
+                    }
+                }
+            }        
+        }
+        return vertex_scc;
+    }
+    
+
+    int calculateMaxJump(Graph tpGraph){
+        // do DFS in original graph
+        list<int> orderedVertices = DFS(1);                  // start DFS at vertex 1
+        vector<int> vertex_scc = calculate_SCC(orderedVertices, tpGraph);
+
+        vector<Color> color(vertices + 1, WHITE);
+        stack<int> vertexStack;
+
+        vector<int> dp(vertices + 1, 0);                     // stores the max jumps for each subproblem (each vertex)   
+        int maxJump = 0;
+
+        for (auto it = orderedVertices.rbegin(); it != orderedVertices.rend(); ++it) {
+            const int& v = *it;
+
+            if (color[v] == WHITE) {
+                vertexStack.push(v);
+
+                while (!vertexStack.empty()) {
+                    int currentVertex = vertexStack.top();
+                    vertexStack.pop();
+
+                    if (color[currentVertex] == WHITE) {
+                        color[currentVertex] = GREY;
+                        vertexStack.push(currentVertex);
+
+                        for (int adjacentVertex : this->adjacencyList[currentVertex]) {
+                            if (color[adjacentVertex] == WHITE){
+                                vertexStack.push(adjacentVertex);
+                            }
+                            if(vertex_scc[adjacentVertex] != vertex_scc[currentVertex]){
+                                dp[currentVertex] = max(dp[currentVertex], dp[adjacentVertex] + 1);
+                            }
+                            else{
+                                dp[currentVertex] = max(dp[currentVertex], dp[adjacentVertex]);
+                            }
                             maxJump = max(maxJump, dp[currentVertex]);  
                         }
-                    } 
+                    }
+
                     else if (color[currentVertex] == GREY) {
                         color[currentVertex] = BLACK;
                     }
                 }
-            }
+            }        
         }
+                
         return maxJump;
     }
-     
-
 };
 
 
