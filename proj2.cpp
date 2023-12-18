f6400f27fe5298a423f02e3edac022dd15aedd48
ist1107294 <rita.de.melo@tecnico.ulisboa.pt>
2023-12-18 11:56:35 +0000

    15 no mooshak

--- proj2.cpp
+++ proj2.cpp
@@ -22,7 +22,7 @@ public:
         adjacencyList[u].push_back(v);
     }
 
-    list<int> DFS(int startVertex) {
+    list<int> DFS() {
         vector<Color> color(vertices + 1, WHITE);
         stack<int> vertexStack;
         list<int> orderedVertices;                           // list to store vertices in descending order of time
@@ -102,7 +102,7 @@ public:
 
     int calculateMaxJump(Graph tpGraph){
         // do DFS in original graph
-        list<int> orderedVertices = DFS(1);                  // start DFS at vertex 1
+        list<int> orderedVertices = DFS();                  // start DFS at vertex 1
         vector<int> vertex_scc = calculate_SCC(orderedVertices, tpGraph);
 
         vector<Color> color(vertices + 1, WHITE);
@@ -153,35 +153,26 @@ public:
 
 
 int main(){
-    int numInd, numConnect;                                  // reads graph information
-    scanf("%d %d", &numInd, &numConnect);                    // num_ind -> num of vertices; num_connect -> num of edges
-    
-    if(numInd < 2 || numConnect < 0) {                       // checks arguments
-        printf("Invalid arguments\n");
-        return 1;
-    }
+    int numInd, numConnect;                                      // reads graph information
+    scanf("%d %d", &numInd, &numConnect);                        // num_ind -> num of vertices; num_connect -> num of edges
+    int maxJump = 0;
 
-    Graph TugaNet(numInd);                                   // create graph
-    Graph transposeTugaNet(numInd);                          // create transpose graph
-    
-    for (int i = 0; i < numConnect; i++){          
-        int u, v;
-        scanf("%d %d", &u, &v);
-        TugaNet.addConnection(u, v);                         // adds the edges to the graph
-        transposeTugaNet.addConnection(v, u);                // adds reverse edges to transpose graph
-    } 
-    
-    int maxJump = TugaNet.calculateMaxJump(transposeTugaNet);
-    printf("%d\n", maxJump);
-
- /*
-    // PRINT PARA VERIFICAR SE A 1ªDFS ESTÁ CERTA ******************************
-    list<int> result = TugaNet.DFS(1);
-    printf("ordem decrescente de tempos finais:\n");
-    for (const int& element : result) {
-        printf("    vertice: %d\n",element);
+    if(numInd < 2 || numConnect < 0) {                           // checks arguments
+        printf("%d\n", maxJump);
     }
-    // *************************************************************************
-*/
+    else{
+        Graph TugaNet(numInd);                                   // create graph
+        Graph transposeTugaNet(numInd);                          // create transpose graph
+        
+        for (int i = 0; i < numConnect; i++){          
+            int u, v;
+            scanf("%d %d", &u, &v);
+            TugaNet.addConnection(u, v);                         // adds the edges to the graph
+            transposeTugaNet.addConnection(v, u);                // adds reverse edges to transpose graph
+        } 
+        
+        maxJump = TugaNet.calculateMaxJump(transposeTugaNet);
+        printf("%d\n", maxJump);
+    }
     return 0;
 }
